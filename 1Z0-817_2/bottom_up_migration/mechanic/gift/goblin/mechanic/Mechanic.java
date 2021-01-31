f63e8a0f9754f33086126d137b1f8f10d65f452a
André Keßler <andre.kessler91@gmx.de>
2021-01-31 15:11:06 +0100

    Chapter 2: bottom up migration

---
+++ 1Z0-817_2/bottom_up_migration/mechanic/gift/goblin/mechanic/Mechanic.java
@@ -0,0 +1,11 @@
+package gift.goblin.mechanic;
+
+import gift.goblin.toolbox.Toolbox;
+
+public class Mechanic {
+    
+	public static void main(String... args) {
+        System.out.println("Mechanic starts repairing...lets look in the toolbox...");
+		Toolbox.open();
+    }    
+}
---
+++ 1Z0-817_2/bottom_up_migration/toolbox/module-info.java
@@ -0,0 +1,3 @@
+module toolbox {
+	exports gift.goblin.toolbox;
+}
